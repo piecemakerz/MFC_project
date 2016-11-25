2fce723dc4d7395b38049c24681edc51b939d3fe
piecemakerz <piecemakerz@naver.com>
2016-11-25 22:26:02 +0900

    first_git

---
+++ Pacman/PacmanView.cpp
@@ -0,0 +1,104 @@
+
+// PacmanView.cpp : CPacmanView 클래스의 구현
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "Pacman.h"
+#endif
+
+#include "PacmanDoc.h"
+#include "PacmanView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CPacmanView
+
+IMPLEMENT_DYNCREATE(CPacmanView, CView)
+
+BEGIN_MESSAGE_MAP(CPacmanView, CView)
+	// 표준 인쇄 명령입니다.
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+END_MESSAGE_MAP()
+
+// CPacmanView 생성/소멸
+
+CPacmanView::CPacmanView()
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+
+}
+
+CPacmanView::~CPacmanView()
+{
+}
+
+BOOL CPacmanView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CPacmanView 그리기
+
+void CPacmanView::OnDraw(CDC* /*pDC*/)
+{
+	CPacmanDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 여기에 원시 데이터에 대한 그리기 코드를 추가합니다.
+}
+
+
+// CPacmanView 인쇄
+
+BOOL CPacmanView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 기본적인 준비
+	return DoPreparePrinting(pInfo);
+}
+
+void CPacmanView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄하기 전에 추가 초기화 작업을 추가합니다.
+}
+
+void CPacmanView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄 후 정리 작업을 추가합니다.
+}
+
+
+// CPacmanView 진단
+
+#ifdef _DEBUG
+void CPacmanView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CPacmanView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CPacmanDoc* CPacmanView::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CPacmanDoc)));
+	return (CPacmanDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CPacmanView 메시지 처리기
---
+++ Pacman/PacmanView.h
@@ -0,0 +1,49 @@
+
+// PacmanView.h : CPacmanView 클래스의 인터페이스
+//
+
+#pragma once
+
+
+class CPacmanView : public CView
+{
+protected: // serialization에서만 만들어집니다.
+	CPacmanView();
+	DECLARE_DYNCREATE(CPacmanView)
+
+// 특성입니다.
+public:
+	CPacmanDoc* GetDocument() const;
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+public:
+	virtual void OnDraw(CDC* pDC);  // 이 뷰를 그리기 위해 재정의되었습니다.
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 구현입니다.
+public:
+	virtual ~CPacmanView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 생성된 메시지 맵 함수
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // PacmanView.cpp의 디버그 버전
+inline CPacmanDoc* CPacmanView::GetDocument() const
+   { return reinterpret_cast<CPacmanDoc*>(m_pDocument); }
+#endif
+
